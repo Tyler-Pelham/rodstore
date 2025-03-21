a5e7613bc812baaa1c43b7ce2d98cc7b7e18c10a
Tyler-Pelham <t.pelham@hotmail.com>
2025-03-20 18:39:53 -0700

    added css for blog page and individual post pages

--- rodstore/blog/urls.py
+++ rodstore/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.blog),
+    path('', views.blog, name="blog"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
--- rodstore/blog/views.py
+++ rodstore/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'blog/post_page.html', {'post': post})
